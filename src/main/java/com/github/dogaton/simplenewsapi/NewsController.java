fd69d2b4f947113b2b071b3120573d277ed901ea
Christina Dollinger <christina.v.dollinger@gmail.com>
2023-03-10 11:45:51 +0100

    add controller and test for counting endpoint

---
+++ src/main/java/com/github/dogaton/simplenewsapi/NewsController.java
@@ -0,0 +1,46 @@
+package com.github.dogaton.simplenewsapi;
+
+import com.github.dogaton.simplenewsapi.record.Article;
+import com.github.dogaton.simplenewsapi.record.News;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/news")
+@RequiredArgsConstructor
+public class NewsController {
+    private final WebClient webClient = WebClient.builder()
+            .defaultHeader("Accept", MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    @Value("${gnews.api.api-version}")
+    private String apiVersion;
+    @Value("${gnews.api.host}")
+    private String host;
+    @Value("${gnews.api.scheme}")
+    private String httpsScheme;
+    @Value("${gnews.api.key}")
+    private String gNewsApiKey;
+
+    @GetMapping
+    @ResponseBody
+    public Flux<Article> getNewsArticles(@RequestParam int count) {
+        return webClient.get()
+                .uri(UriComponentsBuilder.newInstance()
+                        .scheme(httpsScheme)
+                        .host(host)
+                        .path(apiVersion)
+                        .path("/top-headlines")
+                        .queryParam("max", count)
+                        .queryParam("token", gNewsApiKey)
+                        .build()
+                        .toUri())
+                .retrieve()
+                .bodyToFlux(News.class)
+                .flatMapIterable(News::articles);
+    }
+}
---
+++ src/test/java/com/github/dogaton/simplenewsapi/NewsControllerTest.java
@@ -0,0 +1,40 @@
+package com.github.dogaton.simplenewsapi;
+
+import com.github.dogaton.simplenewsapi.record.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class NewsControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void whenNewsCountIsCalledWithFive_ThenReturnFiveArticles() {
+        int count = 5;
+        webTestClient.get().uri("/news?count={count}", count)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Article.class)
+                .hasSize(count)
+                .consumeWith(response -> {
+                    List<Article> articles = response.getResponseBody();
+                    assertNotNull(articles);
+                    assertEquals(count, articles.size());
+                    for (Article article : articles) {
+                        assertNotNull(article.title());
+                        assertNotNull(article.description());
+                        assertNotNull(article.url());
+                    }
+                });
+    }
+
+}
