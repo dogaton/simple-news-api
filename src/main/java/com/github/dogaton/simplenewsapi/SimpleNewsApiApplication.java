6253f5197facc150aeff8954bccdb9508cd9c329
Christina Dollinger <christina.v.dollinger@gmail.com>
2023-03-10 08:11:03 +0100

    initial commit

---
+++ src/main/java/com/github/dogaton/simplenewsapi/SimpleNewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.dogaton.simplenewsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleNewsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dogaton/simplenewsapi/SimpleNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dogaton.simplenewsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
